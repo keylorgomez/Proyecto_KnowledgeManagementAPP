230bfad98e5d720224eebb57acf565b49a8c4cbf
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-22 14:41:51 -0600

    Se crea el atributo mediaId y se crea un constructor que recibe todos los parametros de Media

--- src/main/java/com/horaaventura/horaaventura/Modelo/Media.java
+++ src/main/java/com/horaaventura/horaaventura/Modelo/Media.java
@@ -6,6 +6,13 @@ public class Media {
     private String nombreArchivo;
     private String autor;
     private LocalDate fechaCreacion;
+    private int mediaId;
 
-
+    //Constructor con todos los parametros
+    public Media(String nombreArchivo, String autor, LocalDate fechaCreacion, int mediaId) {
+        this.nombreArchivo = nombreArchivo;
+        this.autor = autor;
+        this.fechaCreacion = fechaCreacion;
+        this.mediaId = mediaId;
+    }
 }
