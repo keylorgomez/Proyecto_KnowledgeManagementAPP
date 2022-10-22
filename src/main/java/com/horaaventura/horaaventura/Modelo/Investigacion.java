bb7804082281b97bce545be70cf9b9ca291029aa
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-22 15:26:21 -0600

    Se crean los atributos correspondientes para la clase Investigación

---
+++ src/main/java/com/horaaventura/horaaventura/Modelo/Investigacion.java
@@ -0,0 +1,20 @@
+package com.horaaventura.horaaventura.Modelo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Investigacion {
+    private int InvestigacionId;
+    private String nombreAutor;
+    private LocalDate fechaInicio;
+    private LocalDate fechaModificacion;
+    private String titulo;
+    private ArrayList<String> subtitulos;
+    public ArrayList<String> textos;
+    public ArrayList<Media> maedia;
+
+
+
+
+
+}
