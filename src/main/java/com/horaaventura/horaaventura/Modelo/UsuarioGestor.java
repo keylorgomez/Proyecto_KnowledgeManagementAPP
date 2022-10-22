3ce740055fa913809531657c933518055e876e69
andreachavesj <achavesji@ucenfotec.ac.cr>
2022-10-22 15:48:27 -0800

    Se crea la clase de UsuarioGestor que hereda los atributos de la clase de Usuario además de un atributo de gestor

---
+++ src/main/java/com/horaaventura/horaaventura/Modelo/UsuarioGestor.java
@@ -0,0 +1,23 @@
+package com.horaaventura.horaaventura.Modelo;
+
+
+import java.time.LocalDate;
+
+public class UsuarioGestor extends Usuario {
+    public UsuarioGestor(String nombre, String apellido, int edad, LocalDate fechaNacimiento, String email, String password, String foto, boolean rolLider, int usuarioId) {
+        super(nombre, apellido, edad, fechaNacimiento, email, password, foto, rolLider, usuarioId);
+        this.gestor = gestor;
+    }
+
+    private boolean gestor;
+
+    public boolean isGestor() {
+        return gestor;
+    }
+
+    public void setGestor(boolean gestor) {
+        this.gestor = gestor;
+    }
+
+}
+
