d48da88e0d6380953dc04b373dd94dc891bc79d1
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-22 14:27:19 -0600

    Se crean los atributos correspondientes para la clase Media

---
+++ src/main/java/com/horaaventura/horaaventura/Modelo/Media.java
@@ -0,0 +1,11 @@
+package com.horaaventura.horaaventura.Modelo;
+
+import java.time.LocalDate;
+
+public class Media {
+    private String nombreArchivo;
+    private String autor;
+    private LocalDate fechaCreacion;
+
+
+}
