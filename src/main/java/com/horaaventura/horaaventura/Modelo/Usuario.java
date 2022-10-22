daf2cef8f986a38544c400b998758cb5ac21c2a6
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-22 13:49:49 -0600

    Se crea la clase Usuario y se agregan sus atributos

---
+++ src/main/java/com/horaaventura/horaaventura/Modelo/Usuario.java
@@ -0,0 +1,18 @@
+package com.horaaventura.horaaventura.Modelo;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private LocalDate fechaNacimiento;
+    private String email;
+    private String password;
+    private String foto;
+    private boolean rolLider;
+    private  int usuarioId;
+
+
+
+}
