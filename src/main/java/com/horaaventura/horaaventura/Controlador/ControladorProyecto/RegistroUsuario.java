427bd3c0c3368f4b9b0c66c2d91a80bf945d099c
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-22 21:21:27 -0600

    Validacion del email y la contraseña con regex

--- src/main/java/com/horaaventura/horaaventura/Controlador/ControladorProyecto/RegistroUsuario.java
+++ src/main/java/com/horaaventura/horaaventura/Controlador/ControladorProyecto/RegistroUsuario.java
@@ -1,4 +1,45 @@
 package com.horaaventura.horaaventura.Controlador.ControladorProyecto;
 
+import com.horaaventura.horaaventura.Modelo.Usuario;
+import javafx.scene.control.Alert;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegistroUsuario {
+    Usuario usuario;
+
+
+
+    public boolean validarUsserandContra(String email, String password){
+        Pattern Correo =null;
+        Matcher ResultadoCorreo=null;
+
+        Correo=Pattern.compile("^([0-9a-zA-Z]([_.w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-w]*[0-9a-zA-Z].)+([a-zA-Z]{2,9}.)+[a-zA-Z]{2,3})$");
+
+        Pattern Contrasenna=null;
+        Matcher ResultadoContra=null;
+
+        Contrasenna=Pattern.compile( "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$");
+        ResultadoCorreo= Correo.matcher(email);
+        ResultadoContra=Contrasenna.matcher(password);
+
+        if(ResultadoCorreo.find()==false){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Incorrecto");
+            alert.setContentText("El formato de correo ingresado no es permitido");
+            alert.showAndWait();
+            return false;
+        }else if(ResultadoContra.find()==false){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Incorrecto");
+            alert.setContentText("La contraseña debe contener de 4 a 8 caracteres. Además incluir mínimo una mayúscula y un número.");
+            alert.showAndWait();
+            return false;
+        }else{
+            return true;}
+
+    }
 }
