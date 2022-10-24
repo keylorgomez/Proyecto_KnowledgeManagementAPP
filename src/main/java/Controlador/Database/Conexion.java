62389ba1b33caa33113cb0d5a1ad94029ed41c21
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-23 20:08:02 -0600

    Se realiza la conexion a la base de datos "proyectopoo"

--- src/main/java/Controlador/Database/Conexion.java
+++ src/main/java/Controlador/Database/Conexion.java
@@ -1,4 +1,31 @@
 package Controlador.Database;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class Conexion {
+
+    private Connection connection;
+    private String usuario="root";
+    private String password="achaves2912";
+    private String servidor="localhost";
+    private String puerto="3306";
+    private String nombreDB="proyectopoo";
+    private String url="jdbc:mysql://"+servidor+":"+puerto+"/"+nombreDB+"?serverTimezone=UTC";
+    private String driver="com.mysql.cj.jdbc.Driver";
+
+    public Conexion(){
+        try {
+            Class.forName(driver);
+            connection=DriverManager.getConnection(url,usuario,password);
+            if (connection!=null){
+                System.out.println("Conexion realiada con éxito");
+            }
+        }catch (Exception e){
+            System.err.println("Ocorrió un error en la conexión");
+            System.err.println("Mensaje del error: "+e.getMessage());
+            System.err.println("Detalle del error: ");
+            e.printStackTrace();
+
+        }
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.horaaventura.horaaventura {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens Vista to javafx.fxml;
     exports Vista;
