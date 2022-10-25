9483a16c8a00e8e9526a013b3b36dd16e3fe19a1
andreachavesj <achavesji@ucenfotec.ac.cr>
2022-10-24 16:24:32 -0800

    Se realiza una validación de campos vacíos para login y en caso de ser así, que muestre mensaje de error y que si no, genere mensaje de exito

--- src/main/java/Controlador/Proyecto/LoginControlador.java
+++ src/main/java/Controlador/Proyecto/LoginControlador.java
@@ -1,11 +1,13 @@
 package Controlador.Proyecto;
 
+import Modelo.Usuario;
 import Vista.Inicio;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,10 +15,12 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class LoginControlador {
-
+    Usuario usuario;
     @FXML private Button btnIniciar;
     @FXML private Button btnRegistrarse;
     @FXML private ImageView imagLogo;
@@ -26,9 +30,17 @@ public class LoginControlador {
     @FXML private Label labelUsuario;
     @FXML private TextField txtContrasenia;
     @FXML private TextField txtUsuario;
+    @FXML private Label mensaje;
 
-    public LoginControlador() {
+
+    public void validarLogin() throws SQLException {
+        if (txtUsuario.getText() == "" || txtContrasenia.getText() == "")
+            mensaje.setText("Datos incorrectos");
+        else {
+            mensaje.setText("Ingreso exitoso");
+        }
     }
+
 
     public void IrRegistrarse(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(Inicio.class.getResource("RegistroUsuario.fxml")));
