465e60b2ce5dfb7bf3c23afa6b855b8cd167fbf9
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-25 16:03:10 -0600

    Probando el registrando proyectos a la DB

--- src/main/java/controlador/proyecto/CrearProyectoControlador.java
+++ src/main/java/controlador/proyecto/CrearProyectoControlador.java
@@ -1,47 +1,89 @@
 package controlador.proyecto;
 
 
+import controlador.dao.ProyectoDao;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.StageStyle;
 import modelo.Proyecto;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class CrearProyectoControlador {
     private Proyecto proyecto;
 
-    @FXML private DatePicker dpFechaCreacion;
-    @FXML private DatePicker dpFechaModificacion;
+    @FXML
+    private Label labelCategoria;
 
-    @FXML private Label labelCategoria;
-
-    @FXML private Label labelColaboradores;
-
-    @FXML private Label labelCrearProyecto;
-
-    @FXML private Label labelFechaCreacion;
-
-    @FXML private Label labelFechaModificacion;
-
-    @FXML private Label labelMedia;
+    @FXML
+    private Label labelCrearProyecto;
 
     @FXML
     private Label labelNombre;
 
-    @FXML private Label labelRepositorio;
+    @FXML
+    private Label labelRepositorio;
 
-    @FXML private TextField txtCategoría;
+    @FXML
+    private TextField txtCategoría;
 
-    @FXML private TextField txtColaboradores;
+    @FXML
+    private TextField txtNombrePoryecto;
 
-    @FXML private TextField txtMedia;
+    @FXML
+    private TextArea txtRepositorio;
 
-    @FXML private TextArea txtRepositorio;
+    private ProyectoDao proyectoDao;
+
+    public CrearProyectoControlador(){
+        proyecto = new Proyecto();
+        proyectoDao=new ProyectoDao();
+    }
 
     @FXML public void crearProtecto(){
-        //proyecto = new Proyecto();
 
+        proyecto.setNombre(txtNombrePoryecto.getText());
+        proyecto.setCategoria(txtCategoría.getText());
+        proyecto.setRepositorio(txtRepositorio.getText());
+
+        String nombreProyecto=proyecto.getNombre();
+        String categoriaProyecto=proyecto.getCategoria();
+        String repositorioProyecto=proyecto.getRepositorio();
+
+        ValidarCampos(nombreProyecto,categoriaProyecto,repositorioProyecto);
+    }
+    public boolean ValidarCampos(String nombre, String categoria, String repositorio){
+        boolean rsp=true;
+        LocalDate fechaCreacion= LocalDate.now();
+        LocalDate ultimaModificacion=LocalDate.now();
+
+        if(nombre.isEmpty() || categoria.isEmpty() || repositorio.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("Error debido a espacios en blanco");
+            alert.showAndWait();
+            rsp=false;
+            return rsp;
+        }else {
+            proyecto=new Proyecto(nombre,categoria,fechaCreacion,ultimaModificacion,repositorio);
+            rsp=proyectoDao.registrarProyecto(proyecto);
+            Alert alert=new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Éxito");
+            alert.setHeaderText(null);
+            alert.setContentText("Se registró correctamente el proyecto");
+            alert.initStyle(StageStyle.UTILITY);
+            alert.showAndWait();
+            limpiarCampos();
+            return rsp;
+        }
+
+    }
+    public void limpiarCampos(){
+        txtNombrePoryecto.setText("");
+        txtCategoría.setText("");
+        txtRepositorio.setText("");
     }
 
 }
