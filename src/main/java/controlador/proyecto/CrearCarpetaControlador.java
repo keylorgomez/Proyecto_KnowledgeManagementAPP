04571bf77411b252cd7a7c475bce2ef51f9947ab
andreachavesj <achavesji@ucenfotec.ac.cr>
2022-10-25 19:48:09 -0800

    Se crea la clase carpeta y se crea su javafx y su controlador y se ingresan los datos ingresados a la base de datos

--- src/main/java/controlador/proyecto/CrearCarpetaControlador.java
+++ src/main/java/controlador/proyecto/CrearCarpetaControlador.java
@@ -1,15 +1,23 @@
 package controlador.proyecto;
-
+import controlador.proyecto.CrearProyectoControlador;
 import controlador.dao.CarpetaDao;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import modelo.Carpeta;
 import modelo.Proyecto;
+import vista.Inicio;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class CrearCarpetaControlador {
     @FXML
@@ -17,15 +25,20 @@ public class CrearCarpetaControlador {
     @FXML
     private TextField txtmedia;
 
+    @FXML
+    private Button btnCrearCarpeta;
+
     private Carpeta carpeta;
     private CarpetaDao carpetaDao;
+
+
 
     public CrearCarpetaControlador() {
         carpeta = new Carpeta();
         carpetaDao = new CarpetaDao();
     }
 
-    @FXML public void crearCarpeta(){
+    @FXML public void crearCarpeta() throws IOException {
 
         carpeta.setInvestigacion(txtinvestigacion.getText());
         carpeta.setMedia(txtmedia.getText());
@@ -36,7 +49,7 @@ public class CrearCarpetaControlador {
         ValidarCamposCarpeta(investigacion, media);
     }
 
-    public boolean ValidarCamposCarpeta(String linkInvestigacion, String linkMedia){
+    public boolean ValidarCamposCarpeta(String linkInvestigacion, String linkMedia) throws IOException {
         //int IDUsuario=LoginControlador.UserIdActivo;
         boolean rsp=true;
 
@@ -57,8 +70,15 @@ public class CrearCarpetaControlador {
             alert.setContentText("Se registró correctamente la carpeta al proyecto");
             alert.initStyle(StageStyle.UTILITY);
             alert.showAndWait();
+            regresarProyecto();
             //limpiarCampos();
             return rsp;
         }
     }
+    public void regresarProyecto() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Inicio.class.getResource("CrearProyecto.fxml")));
+        Stage window = (Stage) btnCrearCarpeta.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
 }
