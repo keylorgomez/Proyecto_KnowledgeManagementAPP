d5f70763c60265af7006e1ca4e6414a164fcd91a
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-11-17 00:23:45 -0600

    Creando la función del botón regresar de la pantalla Espera

--- src/main/java/controlador/proyecto/PantallaEsperaController.java
+++ src/main/java/controlador/proyecto/PantallaEsperaController.java
@@ -1,4 +1,27 @@
 package controlador.proyecto;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import vista.Inicio;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class PantallaEsperaController {
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    void Regresar(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Inicio.class.getResource("Login.fxml")));
+        Stage window = (Stage) btnRegresar.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
 }
