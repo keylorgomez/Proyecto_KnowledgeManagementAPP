3388d2a501d30018aed1f4fef2c06d3cd622d1e3
diego-molina-cenfotec <dmolinat@ucenfotec.ac.cr>
2022-12-05 23:21:41 -0600

    Para obtener el numero de palabras en un documento = se cambio en en Documento Controller el tipo del metodo crearDocumento de de void a File y despues de catch en la linea 30 o 31 se pone a retornar documento (en la linea 11)
    // metodo nuevo
    public static int contarPalabras
    
    en CrearInvestigacionControler :
    
    // el cambio es en las lineas donde se crea la carpetas y el documento
    
    se quita cambpo incluir ruta manualmente de carpeta investigacion  en fxml y en Investigacioncontroller

--- src/main/java/controlador/proyecto/DocumentoController.java
+++ src/main/java/controlador/proyecto/DocumentoController.java
@@ -1,13 +1,14 @@
 package controlador.proyecto;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class DocumentoController {
 
     public DocumentoController() {
     }
 
-    public static void crearDocumento(String fechaModificacion, String fechaInicial, String ruta, String categoria, String tema, String autor, String titulo,
+    public static File crearDocumento(String fechaModificacion, String fechaInicial, String ruta, String categoria, String tema, String autor, String titulo,
                                       String contenido1, String subTitulo, String contenido2){
         String  rutaNombre = ruta+"\\"+titulo+".rtf";
         File documento = new File(rutaNombre);
@@ -27,7 +28,33 @@ public class DocumentoController {
         } catch (FileNotFoundException ex) {
             ex.printStackTrace(System.out);
         }
+        return documento;
     }
+
+    public static int contarPalabras(File docInvestigacion) throws IOException {
+        int palabrasTotales=0;
+        File documento = new File(docInvestigacion.toURI());
+        try {
+            if (documento.exists()){
+                BufferedReader archivoLeer = new BufferedReader(new FileReader(documento));
+                String lineaLeida;
+
+                while ((lineaLeida = archivoLeer.readLine()) != null){
+                    System.out.println(lineaLeida);
+                    // contamos las palabras
+                    StringTokenizer st = new StringTokenizer(lineaLeida);
+                    palabrasTotales= palabrasTotales + st.countTokens();
+                }
+                System.out.println("\nPalabras totales es: "+ palabrasTotales);
+                archivoLeer.close();
+            }else {
+                System.out.println("no se encuetra archivo");
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+        return palabrasTotales;
+    }// fin de contar palabras
 
     public static void agregarInformacion(String nombreDocumento, String subtitulo, String contenido){
         File documento =  new File(nombreDocumento);
